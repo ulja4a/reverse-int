6913597a720310c284cd9b5a75c9823d9a852e87
ulja4a <ulja4a@gmail.com>
2023-02-12 23:02:42 +0400

    passing test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  let x = Array.from(String(Math.abs(n)), Number);
+  console.log(x);
+  let Reverse = (x.reverse()).join('');
+  let resltReverse = Number(Reverse);
+  return resltReverse;
 }
